9efee3e0117b3a3865533c78bbaf1737a91a9e77
Nagaraju Matta <nagaraju.matta@infomagnus.com>
2024-04-26 16:31:36 +0530

    updated the env values

--- src/api/GitHubApi.ts
+++ src/api/GitHubApi.ts
@@ -13,17 +13,28 @@ export const getGitHubCopilotMetricsApi = async (): Promise<Metrics[]> => {
   
   let response;
   let metricsData;
+  interface EnvVariables {
+    gh_token: string;
+    gh_orgName: string;
+    mocked_data: boolean;
+  }
+
+  const envVariables: EnvVariables = {
+    gh_token: process.env.VUE_APP_GITHUB_TOKEN || '',
+    gh_orgName: process.env.VUE_APP_GITHUB_ORG || '',
+    mocked_data: process.env.VUE_APP_MOCKED_DATA === "true",
+  };
 
   if (process.env.VUE_APP_MOCKED_DATA === "true") {
     response = data;
     metricsData = response.map((item: any) => new Metrics(item));
   } else {
     response = await axios.get(
-      `https://api.github.com/orgs/im-infomagnus/copilot/usage`,
+      `https://api.github.com/orgs/${envVariables.gh_orgName}/copilot/usage`,
       {
         headers: {
           Accept: "application/vnd.github+json",
-          Authorization: `Bearer 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`,
+          Authorization: `Bearer ${envVariables.gh_token}`,
           "X-GitHub-Api-Version": "2022-11-28",
         },
       }
